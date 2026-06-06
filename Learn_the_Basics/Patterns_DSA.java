48ac4108e4692355b591d18a1aeb369783fafeb9
SuperAnand67 <anand1a2b@gmail.com>
2026-06-07 00:14:20 +0530

    Learned Mathematical Formulas for patterns

--- Learn_the_Basics/Patterns_DSA.java
+++ Learn_the_Basics/Patterns_DSA.java
@@ -28,7 +28,6 @@ class Patterns{
             }
             System.out.println();
         }
-        System.out.println();
     }
 
     /** Right-Angled Number Pyramid
@@ -71,6 +70,16 @@ class Patterns{
         System.out.println();
     }
 
+    public void pattern5Variant(int n){
+        for(int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
     public void pattern6(int n){
         for (int i = n; i > 0; i--) {
             for (int j = 1; j <= i; j++) {
@@ -81,34 +90,49 @@ class Patterns{
         System.out.println();
     }
 
-    public void pattern7(int n){
+    public void pattern6Variant(int n){
         for (int i = 0; i < n; i++) {
-            for(int j = 0; j < n - i - 1; j++) {
-                System.out.print(" ");
-            }
-
-            for (int j = 0; j < 2 * i + 1; j++) {
-                System.out.print("*");
-            }
-
-            for (int j = 0; j < n - i - 1; j++) {
-                System.out.print(" ");
+            for (int j = 0; j < n - i; j++) {
+                System.out.print((j+1) + " ");
             }
             System.out.println();
         }
         System.out.println();
     }
 
+    public void pattern7(int n){
+        for (int i = 0; i < n; i++) {
+            // spaces
+            for(int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            // spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
     public void pattern8(int n){
         for(int i = 0;i < n; i++){
+            // spaces
             for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
 
+            // stars
             for (int j = 0; j < 2 * (n - i) - 1 ; j++) {
                 System.out.print("*");
             }
 
+            // spaces
             for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
@@ -119,32 +143,59 @@ class Patterns{
     }
 
     // It combines Pattern 7 and 8 to make a Diamond Shape Patterns
-    public void pattern9Combination(int n) {
-        for (int i = 0; i < n; i++) {
-            for(int j = 0; j < n - i - 1; j++) {
-                System.out.print(" ");
-            }
+    public void pattern9(int n) {
+        pattern7(n); // Upper Triangle
+        pattern8(n); // Upside Down Triangle
+    }
 
-            for (int j = 0; j < 2 * i + 1; j++) {
-                System.out.print("*");
-            }
+    // It combines Pattern 2 and 5 to make a Arrow like Shape
+    //Either one of those patterns must n - 1 times
+    public void pattern10(int n){
+        pattern2(n - 1);
+        pattern5(n);
+    }
 
-            for (int j = 0; j < n - i - 1; j++) {
-                System.out.print(" ");
+    public void pattern10Variant(int n){
+        for (int i = 0; i < 2 * n - 1; i++) {
+            int s = (i >= n) ? (2 * n - i - 1) : i+1;
+
+            for (int j = 0; j < s; j++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
-        for(int i = 0;i < n; i++){
-            for (int j = 0; j < i; j++) {
+        System.out.println();
+    }
+
+    public void pattern11(int n){
+        for (int i = 0; i < n; i++) {
+            int start = (i % 2 == 0) ? 1 : 0;
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(start + " ");
+                //start = (start == 0) ? 1 : 0;
+                start = 1 - start;
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    
+    public void pattern12(int n){
+        for (int i = 0; i < n; i++) {
+            // nums
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j+1);
+            }
+
+            // spaces
+            for (int j = 0; j < 2 * n - 2 * (i + 1); j++) {
                 System.out.print(" ");
             }
 
-            for (int j = 0; j < 2 * (n - i) - 1 ; j++) {
-                System.out.print("*");
-            }
-
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
+            // nums
+            for (int j = i+1 ; j > 0; j--) {
+                System.out.print(j);
             }
 
             System.out.println();
@@ -152,6 +203,18 @@ class Patterns{
         System.out.println();
     }
 
+    public void pattern13(int n){
+        int c = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(c + " ");
+                c++;
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
 
 public class Patterns_DSA {
@@ -172,13 +235,19 @@ public class Patterns_DSA {
 
         ptn.pattern1(n);
         ptn.pattern2(n);
+        System.out.println();
         ptn.pattern3(n);
         ptn.pattern4(n);
-        ptn.pattern5(n);
-        ptn.pattern6(n);
+        ptn.pattern5Variant(n);
+        ptn.pattern6Variant(n);
         ptn.pattern7(n);
+        System.out.println();
         ptn.pattern8(n);
         ptn.pattern9(n);
+        ptn.pattern10Variant(n);
+        ptn.pattern11(n);
+        ptn.pattern12(n);
+        ptn.pattern13(n);
 
         sc.close();
     }    
